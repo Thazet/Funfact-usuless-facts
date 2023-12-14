6f41e4d117e937be434abcb55ce6980c8cc92b9b
annaLucian <garielapoves01@gmail.com>
2023-12-14 15:33:04 +0100

    cambiando estructura de carpetas

---
+++ tests/app.test.js
@@ -0,0 +1,6 @@
+import { expect, it } from 'vitest'
+describe('APP', () => {
+    it("", () => {
+        expect(2 * 2).toEqual(4)
+    })
+})
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+
+
+    //Añadimos soporte al navegador
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './tests/setup.js',
+        css: true,
+    }
+})
