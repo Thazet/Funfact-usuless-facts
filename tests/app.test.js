2215478c65c13d7a87581c8238216f1ca4f52a8f
debora-smb <debora.smborges@gmail.com>
2023-12-18 15:39:05 +0100

    add fact-section tests and update app tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,26 +1,28 @@
 import { expect, it, describe, beforeAll } from 'vitest'
-import {JSOM} from 'jsdom'
-
-import "./app.test.js";
+import { JSDOM } from 'jsdom'
+import { addFavoriteFact, getRamdomFacts } from '../src/js/app.js';
 
 describe('pageTest', () => {
-    let dom; 
-    beforeAll(async () =>{
+    let dom;
+    beforeAll(async () => {
         dom = await JSDOM.fromFile("./index.html", {
-        resources: "usable",
-        runScripts: "dangerously",
-    });
+            resources: "usable",
+            runScripts: "dangerously",
+        });
 
-})
-it("should generate new fact"), async () => {
-    document = dom.window.document;
-    const btnFact = document.querySelector (".carousel__change-image-button")
+    })
+    it("should generate new fact"), async () => {
+        document = dom.window.document;
+        const btnFact = document.querySelector(".carousel__change-image-button");
+        const testArr = ["The average person falls asleep in seven minutes.", "Reindeer like to eat bananas."];
+        expect(btnFact(testArr)).toEqual(["The average person falls asleep in seven minutes."]);
+    }
 
-    const testArr = [ "The average person falls asleep in seven minutes.", "Reindeer like to eat bananas."];
-        
+    it("should exist addFavouriteFact function"), async () => {
+        expect(addFavoriteFact()).toBeDefined();
+    }
 
-expect (btnFact(testArr)).toEqual ([ "The average person falls asleep in seven minutes."]);
-}
-
-
+    it("should exist getRamdomFacts function"), () => {
+        expect(getRamdomFacts()).toBeDefined();
+    }
 })
